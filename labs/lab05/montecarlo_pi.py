404dec95e84b6ef0ca73ebc568099842c799ba46
Santiago Bonada <Santiago.Bonada@uoit.net>
2017-11-01 23:52:51 -0400

    hugely overlooked bug I never saw

--- labs/lab05/montecarlo_pi.py
+++ labs/lab05/montecarlo_pi.py
@@ -7,7 +7,7 @@ def estimate_pi(n_samples,rnd_seed=0):
     np.random.seed(rnd_seed)
     samples_x = np.random.uniform(0,1,n_samples)
     samples_y = np.random.uniform(0,1,n_samples)
-    distances = np.sqrt(np.power(samples_x,2) + np.power(samples_y**2,2))
+    distances = np.sqrt(np.power(samples_x,2) + np.power(samples_y,2))
     return 4.0 * np.sum(distances <= 1.0) / float(n_samples)
 
 
